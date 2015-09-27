b9b4b0a5b3f3ae214371302b9d49ec9068064df8
neetu.pradhan <neetu@miracleinterface.com>
2015-09-27 20:09:31 +0545

    [Code Managed] - controller -> category - views/admin/category/list.php

--- application/controllers/admin/category.php
+++ application/controllers/admin/category.php
@@ -1,11 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Category extends CI_Controller {
+class category extends CI_Controller {
     public function __construct()
     {
         parent::__construct();
-        $this->load->model('category_model');
+        $this->load->model('admin/category_model');
         $this->load->library('form_validation');
         $this->helper_model->validate_session();
 
