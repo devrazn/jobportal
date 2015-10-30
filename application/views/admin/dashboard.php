ec6dc9e0c8e15db339dfdcf182137e5a2e871bd6
rajan <acharya.rajanpkr@gmail.cokm>
2015-10-31 01:06:15 +0545

    adjusted height of dashboard icons

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -11,52 +11,52 @@
               <div class="container-fluid" id="pcont">
                 <div class="stats_bar">
                     <div class="butpro butstyle">
-                      <div class="sub"><h4>Admin Users</h4></div>
+                      <div class="sub"><h6>Admin Users</h6></div>
                       <div class="stat"><a href="<?=base_url().'admin/user';?>"><img src="<?=base_url()?>images/users.png" /></a> </div>
                     </div>
 
                     <div class="butpro butstyle">
-                      <div class="sub"><h4>Site Setting</h4></div>
+                      <div class="sub"><h6>Site Setting</h6></div>
                       <div class="stat"><a href="<?=base_url().'admin/settings';?>"><img src="<?=base_url()?>images/site_setting.png" /></a> </div>
                     </div>
 
                     <div class="butpro butstyle">
-                        <div class="sub"><h4>Contact Details</h4></div>
+                        <div class="sub"><h6>Contact Details</h6></div>
                         <div class="stat"><a href="<?=base_url().'admin/settings/contact_details';?>"><img src="<?=base_url()?>images/search_index.png" /></a> </div>
                     </div>
 
                     <div class="butpro butstyle">
-                      <div class="sub"><h4>Change password</h4></div>
+                      <div class="sub"><h6>Change password</h6></div>
                       <div class="stat"><a href="<?=base_url().'admin/settings/change_password';?>"><img src="<?=base_url()?>images/change_password.png" /></a> </div>
                     </div>  
 
                     <div class="butpro butstyle">
-                        <div class="sub"><h4>Mail Settings</h4></div>
+                        <div class="sub"><h6>Mail Settings</h6></div>
                         <div class="stat"><a href="<?=base_url().'admin/settings/email_templates';?>"><img src="<?=base_url()?>images/mail_settings.png" /></a> </div>
                     </div>
 
                     <div class="butpro butstyle">
-                        <div class="sub"><h4>Newsletter</h4></div>
+                        <div class="sub"><h6>Newsletter</h6></div>
                         <div class="stat"><a href="<?=base_url().'admin/newsletter';?>"><img src="<?=base_url()?>images/newsletter.png" /></a> </div>
                     </div>
 
                     <div class="butpro butstyle">
-                      <div class="sub"><h4>Cms</h4></div>
+                      <div class="sub"><h6>Cms</h6></div>
                       <div class="stat"><a href="<?=base_url().'admin/settings/cms';?>"><img src="<?=base_url()?>images/cms.png" /></a> </div>
                     </div> 
 
                     <div class="butpro butstyle">
-                      <div class="sub"><h4>Job Category</h4></div>
+                      <div class="sub"><h6>Job Category</h6></div>
                       <div class="stat"><a href="<?=base_url().'admin/category';?>"><img src="<?=base_url()?>images/category.png" /></a> </div>
                     </div>
 
                     <div class="butpro butstyle">
-                      <div class="sub"><h4>Tags Management</h4></div>
+                      <div class="sub"><h6>Tags Management</h6></div>
                       <div class="stat"><a href="<?=base_url().'admin/tags';?>"><img src="<?=base_url()?>images/photo_gallery.png" /></a> </div>
                     </div>
 
                     <div class="butpro butstyle">
-                      <div class="sub"><h4>Registered Users</h4></div>
+                      <div class="sub"><h6>Registered Users</h6></div>
                       <div class="stat"><a href="<?=site_url(ADMIN_PATH.'/member_new')?>"><img src="<?=base_url()?>images/member.png" /></a> </div>
                     </div>    
                 </div>
